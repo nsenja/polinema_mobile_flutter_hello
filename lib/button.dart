50919061488329158617ac006b0bfaa990ac7aad
nsenja <nabilasw34@gmail.com>
2021-10-02 13:23:12 +0700

    Create Button

---
+++ lib/button.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+// Add your onPressed code here!
+          },
+          child: Icon(Icons.thumb_up),
+          backgroundColor: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
